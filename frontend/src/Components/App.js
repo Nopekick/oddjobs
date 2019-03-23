ff9881d6eed883b782cc3c793246bbe063e7fcf2
Josh <joshualawson8@gmail.com>
2019-03-23 16:00:00 -0700

    Fixed merge error

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -5,16 +5,10 @@ import Navbar from './Navbar/navbar'
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <h1>test</h1>
-=======
       <div>
         <Navbar/>
->>>>>>> 587ab4fc0417518ede0e44ada69884e12da26043
       </div>
-    );
-  }
+    )};
 }
 
 
