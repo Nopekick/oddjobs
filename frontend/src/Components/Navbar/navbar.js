711a35e15f44920ff491717eab44a46765c17f38
Josh <joshualawson8@gmail.com>
2019-03-23 16:50:29 -0700

    Fixed navbar navigation

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -1,10 +1,12 @@
 import "./navbar.css"
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
 
 class Navbar extends React.Component {
   render() {
     return <div className="topnav">
-            <span><p id="icon"> OdderJobs</p></span>
+            <span><Link to="/"> Home </Link></span>
             <span><p id="icon"> Search</p></span>
             <span><p id="icon"> Profile Picture</p></span>
 
