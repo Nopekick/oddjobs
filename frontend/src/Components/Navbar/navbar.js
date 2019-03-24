4ec8b7c5cd2dfa2dcf9499b10998d098df40c382
Josh <joshualawson8@gmail.com>
2019-03-23 18:43:54 -0700

    I give up on navbar

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -5,24 +5,14 @@ import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
   render() {
-<<<<<<< HEAD
-          return <div>
-              <img className="image" src={ require('./otter-md.png') } alt="."/>
-                <div className="topnav">
-                <Link to="/"> OdderJobs </Link>
-                <a>Search</a>
-                <Link to="Profile"><img src="" alt="."/></Link>
-=======
-    return  <div className="topnav">
-              <div className="odder">
-              <Link to="/"> OdderJobs </Link>
->>>>>>> b90c144d34aff71469e912ee7fa091a42fd31db0
-              </div>
-            </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> b90c144d34aff71469e912ee7fa091a42fd31db0
+          return <div className="topnav">
+                  <img className="image" src={ require('./otter-md.png') } alt="otter you glad you chose us"/>
+                  <Link to="/"> OdderJobs </Link>
+                  <Link to="/about"> About us </Link>
+                  <Link to="/signin"> Sign in </Link>
+                  <Link to="/signup"> Sign up </Link>
+                <img className="profilePic" src="" alt=""/>
+                </div>
   }
 }
 export default Navbar;
