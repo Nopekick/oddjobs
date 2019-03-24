66403a9e70134437e44e83b57ec73e77ac26c95a
Josh <joshualawson8@gmail.com>
2019-03-23 18:26:51 -0700

    Adding image, still fixing navbar

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -5,22 +5,14 @@ import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
   render() {
-<<<<<<< HEAD
-    return  <div className="topnav">
-              <div className="odder">
-              <Link to="/"> OdderJobs </Link>
+          return <div>
+              <img className="image" src={ require('./otter-md.png') } alt="."/>
+                <div className="topnav">
+                <Link to="/"> OdderJobs </Link>
+                <a>Search</a>
+                <Link to="Profile"><img src="" alt="."/></Link>
               </div>
-              <a>Search</a>
-              <Link to="Profile"><img src="" alt="."/></Link>
             </div>
-=======
-    return <div className="topnav">
-            <div id= "odder"><Link to="/"> OdderJobs </Link></div>
-            <a>Search</a>
-            <Link to="Profile">Profile Picture</Link>
-            </div>
-
->>>>>>> d646d434c880b2a97d8c62a8c95dc2e5e4732740
   }
 }
 export default Navbar;
