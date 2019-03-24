824bdace8715af84b6c6776b2aaf06baa346db5f
Josh <joshualawson8@gmail.com>
2019-03-23 18:47:10 -0700

    Fixing nav

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -5,18 +5,6 @@ import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
   render() {
-<<<<<<< HEAD
-          return <div>
-              <img className="image" src={ require('./otter-md.png') } alt="."/>
-                <div className="topnav">
-                <Link to="/"> OdderJobs </Link>
-                <a>Search</a>
-                <Link to="Profile"><img src="" alt="."/></Link>
-
-              </div>
-            </div>
-
-=======
           return <div className="topnav">
                   <img className="image" src={ require('./otter-md.png') } alt="otter you glad you chose us"/>
                   <Link to="/"> OdderJobs </Link>
@@ -25,7 +13,6 @@ class Navbar extends React.Component {
                   <Link to="/signup"> Sign up </Link>
                   <img className="profilePic" src="" alt=""/>
                 </div>
->>>>>>> 4ec8b7c5cd2dfa2dcf9499b10998d098df40c382
   }
 }
 export default Navbar;
