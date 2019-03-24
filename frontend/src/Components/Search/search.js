5e69c83b407df9b765a06af9a56d50ea37d54de8
Thomas Kamm <thomaskamm@Thomas-MacBook-Pro.local>
2019-03-24 00:07:50 -0700

    render job

--- frontend/src/Components/Search/search.js
+++ frontend/src/Components/Search/search.js
@@ -11,23 +11,31 @@ class Search extends Component {
     }
   }
 
- componentWillMount(){
+ componentDidMount(){
    apiCall("get", 'http://localhost:8081/api/m/jobs/all')
-   .then((jobs)=>{
+   .then(({jobs})=>{
      this.setState({jobs})
+     console.log(jobs)
+   }).catch((err)=>{
+       console.log(err)
    })
  }
 
   render() {
-  this.state.jobs.map((job)=>{
-    return <div>
-        
-    </div>
-  })
+      let jobList = this.state.jobs.map((job, index)=>{
+        let {title, description, timeEstimate, pay, employer} = job
+        let {fname, lname} = employer
+        return <div key={job._id}>
+          <h1>{title}<h2> offered by <em>{fname} {lname}</em></h2></h1>
+          <h2> Approximately {timeEstimate} for {pay} </h2>
+          <h2> Description: {description}</h2>
+        </div>
+      })
 
   return (
     <div>
-
+        <h1> Here </h1>
+          {jobList}
     </div>
     );
   }
