71db7da9608bda0446bc2b6bea0ec0c9d9dc41c7
Scott Lin <scottj.lin@gmail.com>
2019-03-23 18:44:21 -0700

    css for this

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -5,24 +5,16 @@ import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
   render() {
-<<<<<<< HEAD
           return <div>
               <img className="image" src={ require('./otter-md.png') } alt="."/>
                 <div className="topnav">
                 <Link to="/"> OdderJobs </Link>
                 <a>Search</a>
                 <Link to="Profile"><img src="" alt="."/></Link>
-=======
-    return  <div className="topnav">
-              <div className="odder">
-              <Link to="/"> OdderJobs </Link>
->>>>>>> b90c144d34aff71469e912ee7fa091a42fd31db0
+
               </div>
             </div>
-<<<<<<< HEAD
-=======
 
->>>>>>> b90c144d34aff71469e912ee7fa091a42fd31db0
   }
 }
 export default Navbar;
