062482ca964ddf1aeedf6e2f053f5e616cf54149
Thomas Kamm <thomaskamm@Thomas-MacBook-Pro.local>
2019-03-23 23:35:32 -0700

    search

--- frontend/src/Components/Search/search.js
+++ frontend/src/Components/Search/search.js
@@ -2,23 +2,28 @@ import React, {Component} from 'react'
 import {apiCall,setTokenHeader} from "../../service"
 import {Link, Redirect, withRouter} from 'react-router-dom'
 
+// title, description, employer, pay, time estimate
 class Search extends Component {
   constructor(props){
     super(props)
     this.state = {
-      people: []
+      jobs: []
     }
   }
 
-// componentWillMount(){
-//   apiCall("get", `http://localhost:8081/api/m/${this.props.category}/all`)
-//   .then((people)=>{
-//     this.setState({people})
-//   })
-// }
+ componentWillMount(){
+   apiCall("get", 'http://localhost:8081/api/m/jobs/all')
+   .then((jobs)=>{
+     this.setState({jobs})
+   })
+ }
 
   render() {
-
+  this.state.jobs.map((job)=>{
+    return <div>
+        
+    </div>
+  })
 
   return (
     <div>
