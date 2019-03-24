1249781d6e4ab3204846985a0afcc8d78930a393
Josh <joshualawson8@gmail.com>
2019-03-23 19:43:09 -0700

    added frontend

---
+++ frontend/src/Components/bottom/bottom.js
@@ -0,0 +1,12 @@
+import "./bottom.css"
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
+
+
+class bottom extends React.Component {
+  render() {
+          return <div>
+          </div>
+  }
+}
