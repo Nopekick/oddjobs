c2a750c197e657761cf5399d2b4f3be0bcc7cbda
Scott Lin <scottj.lin@gmail.com>
2019-03-24 04:30:15 -0700

    merge

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -4,12 +4,8 @@ import Navbar from './Navbar/navbar'
 import Search from './Search/search'
 import SignIn from './SignIn/signin'
 import SignUp from './SignUp/signup'
-<<<<<<< HEAD
 import Bottom from './Bottom/bottom'
 import OpenRequest from './OpenRequest/openrequest'
-=======
-import Bottom from './bottom/bottom'
->>>>>>> 42fd06a104981097ff9c545dd88ba4be0f6757f5
 import Homepage from './Homepage/homepage'
 import "../index.css"
 import {setTokenHeader} from "../service.js"
@@ -39,13 +35,8 @@ class App extends Component {
         <Switch>
           <Route path="/signup" component={()=> <SignUp isAuthenticated={this.state.isAuthenticated} signIn={this.userLoggedIn} /> } />
           <Route path="/signin" component={()=> <SignIn /> } />
-<<<<<<< HEAD
-          <Route path="/students" component={()=> <Search category="student" />} />
-          <Route path="/employers" component={()=> <Search category="employer" />} />
           <Route path="/employer/request" component={()=> <OpenRequest />} />
-=======
           <Route path="/job-openings" component={()=> <Search  />} />
->>>>>>> 42fd06a104981097ff9c545dd88ba4be0f6757f5
           <Route path="/" component={()=> <Homepage  />} />
         </Switch>
         <Bottom/>
