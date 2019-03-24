81f84154ee893078a31f5075d0f06422505795c0
Scott Lin <scottj.lin@gmail.com>
2019-03-23 17:38:33 -0700

    merge

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -6,12 +6,10 @@ import {Link} from 'react-router-dom'
 class Navbar extends React.Component {
   render() {
     return <div className="topnav">
-            <span><Link to="/"> OdderJobs </Link></span>
-            <span id="search"><Link to="/"> Search </Link></span>
-            <span id="profile"><Link to="Profile">Profile Picture</Link></span>
-
-
-            </div>
+      <span><Link to="/"> OdderJobs </Link></span>
+      <span id="search"><Link to="/"> Search </Link></span>
+      <span id="profile"><Link to="Profile">Profile Picture</Link></span>
+    </div>
   }
 }
 export default Navbar;
