b3877acc5027fe5bd68ad3f8654384d924a6945e
Josh <joshualawson8@gmail.com>
2019-03-23 18:10:59 -0700

    Slowly getting nav bar to work

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -5,18 +5,13 @@ import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
   render() {
-    return <div className="topnav">
-<<<<<<< HEAD
-            <div id= "odder"><Link to="/"> OdderJobs </Link></div>
-            <a>Search</a>
-            <Link to="Profile">Profile Picture</Link>
+    return  <div className="topnav">
+              <div className="odder">
+              <Link to="/"> OdderJobs </Link>
+              </div>
+              <a>Search</a>
+              <Link to="Profile"><img src="" alt="."/></Link>
             </div>
-=======
-      <span><Link to="/"> OdderJobs </Link></span>
-      <span id="search"><Link to="/"> Search </Link></span>
-      <span id="profile"><Link to="Profile">Profile Picture</Link></span>
-    </div>
->>>>>>> ed1614a777d8de39df6205a4e9504f8fb8bad680
   }
 }
 export default Navbar;
