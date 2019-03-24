2162870342eab0df6d3ad60dee47b4b2ae92da74
Josh <joshualawson8@gmail.com>
2019-03-23 17:36:53 -0700

    Trying to fix navbar

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 class Navbar extends React.Component {
   render() {
     return <div className="topnav">
-            <span id="odder"><Link to="/"> OdderJobs </Link></span>
+            <span><Link to="/"> OdderJobs </Link></span>
             <span id="search"><Link to="/"> Search </Link></span>
             <span id="profile"><Link to="Profile">Profile Picture</Link></span>
 
